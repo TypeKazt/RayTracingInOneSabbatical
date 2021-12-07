8bc9ec9d3a7b4d5db37167e8fddb4abe9dde8587
TypeKazt <alexander.v.kazantsev@gmail.com>
2021-12-07 16:02:40 -0500

    more interfaces and vec3

--- src/PPMFormatRender.h
+++ src/PPMFormatRender.h
@@ -2,13 +2,23 @@
 
 #include "RenderIface.h"
 
+class PPMRenderConfig : public RenderConfigIface
+{
+    
+};
+
 class PPMFormatRender: public RenderIface
 {
-
 public:
     void render();
-    void setImageWidthHeight(uint64_t _width, uint64_t _height);
-    void setPixel(uint64_t _width, uint64_t _height, );
+    void setImageDim(uint64_t _width, uint64_t _height);
+    void configRender(RenderConfigIface& _config);
 
-
+    void setPixel(uint64_t _row, uint64_t _col, vec3& vec3);
+    void setPixelRow(uint64_t _row, vector<vec3>& _data);
+    void setPixelCol(uint64_t _col, vector<vec3>& _data);
+    void setImage(std::vector<std::vector<vec3>>& _data);
+ 
+private:
+    PPMRenderConfig m_config;
 };
--- src/RenderIface.h
+++ src/RenderIface.h
@@ -1,25 +1,34 @@
 #pragma once
 
+#include "vec3.h"
+
 #include "stdint.h"
 #include <vector>
-#include "vec3.h"
 
 
 class RenderConfigIface
 {
 public:
+    setImageDim(uint64_t _width, uint64_t _height) { m_imageWidth = _width; m_imageHeight = _height; }
+    uint64_t getImageWidth() { return m_imageWidth; }
+    uint64_t getImageHeight() { return m_imageHeight; }
 
-private:
-
+protected:
+    uint64_t m_imageWidth;
+    uint64_t m_imageHeight;
 };
 
 class RenderIface
 {
 public:
     void render();
-    void setImageWidthHeight(uint64_t _width, uint64_t _height);
-    void setPixel(uint64_t _width, uint64_t _height, );
-    void setPixelRow(uint64_t _row, vector<vec3>& _data);
-    void setPixelRow(uint64_t _col, vector<vec3>& _data);
-    void configRender(RenderConfig& _config);
+    void configRender(RenderConfigIface& _config);
+
+    void setPixel(uint64_t _row, uint64_t _col, vec3& vec3);
+    void setPixelRow(uint64_t _row, std::vector<vec3>& _data);
+    void setPixelCol(uint64_t _col, std::vector<vec3>& _data);
+    void setImage(std::vector<std::vector<vec3>>& _data);
+
+protected:
+    std::vector<std::vector<vec3>> m_image;
 };
