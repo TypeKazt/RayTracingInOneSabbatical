69e4d94015e5809b197b0556476d1da91badb197
Alexander Kazantsev <alexander.v.kazantsev@gmail.com>
2021-12-26 18:10:19 -0500

    clang sucks

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,19 @@
+#include "rtsabbatical.h"
+
+#include "hittable_list.h"
+#include "sphere.h"
 #include "PPMFormatRender.h"
-#include "ray.h"
-#include "vec3.h"
 
 #define CLAMP(X,Y) X > Y ? Y : X
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant) ) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-	auto sphereC = point3(0,0,-1);
-    auto t = hit_sphere(sphereC, 0.5, r);
-	if(t > 0.0)
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if(world.hit(r, 0, infinity, rec))
 	{
-		auto N = unit_vector(r.at(t) - sphereC);
-		//return ((N+vec3(1,1,1))*0.5)*255.99;
-        return (0.5*color(N.x()+1, N.y()+1, N.z()+1))*255.99;
+		return ((rec.normal+vec3(1,1,1))*0.5)*255.99;
 	}
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
+    auto t = 0.5*(unit_direction.y() + 1.0);
     vec3 ray_col = (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 	return ray_col*vec3(255.99, 255.99, 255.99);
 }
@@ -39,6 +24,13 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// World
+	hittable_list world;
+	//world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+	world.add(shared_ptr<sphere>(new sphere(point3(0,0,-1), 0.5)));
+	//world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+	world.add(shared_ptr<sphere>(new sphere(point3(0,-100.5,-1), 100)));
 
     // Camera
 
@@ -68,7 +60,7 @@ int main()
             auto u = double(i) / (image_width-1);
             auto v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
 			ppm.setPixel((image_height-1)-j, i, pixel_color);
 		}
 	}
